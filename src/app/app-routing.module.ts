81047f0a74c4be5072cefc73ac63a69d16223472
Roman Nichi <Roman.Nichi.O@gmail.com>
2024-01-31 21:22:31 +0400

    config app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from 'src/pages/main-page/main-page.component';
+import { PagesModule } from 'src/pages/pages.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
