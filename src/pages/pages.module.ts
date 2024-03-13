1fd332005b43cca820be5bf3b0d8675b4c160761
Marika777 <marika.meidra2003@gmail.com>
2024-03-13 16:03:26 +0400

    header and footer

--- src/pages/pages.module.ts
+++ src/pages/pages.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SwiperModule } from 'swiper/angular';
+import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, SwiperModule],
 })
 export class PagesModule {}
