a04ee1a16d73e6b97ce35a9655fc9ea5913c311f
Marika777 <marika.meidra2003@gmail.com>
2024-03-13 17:36:43 +0400

    { path: '', component: MainPageComponent },

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageComponent } from 'src/pages/about-us-page/about-us-page.component';
 import { MainPageComponent } from 'src/pages/main-page/main-page.component';
 import { PagesModule } from 'src/pages/pages.module';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
+  { path: 'about-us', component: AboutUsPageComponent },
 ];
 
 @NgModule({
