72aaaeccfce151cbd7d0ccf896268fc153854d6b
Roman Nichi <Roman.Nichi.O@gmail.com>
2024-03-15 09:33:34 +0400

    move questions in shared module

--- src/components/shared-components.module.ts
+++ src/components/shared-components.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CodeEditorModule } from '@ngstack/code-editor';
+
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { LoadingComponent } from './loading/loading.component';
+import { QuestionComponent } from './question/question.component';
+import { QuestionCodeComponent } from './question/question-types/question-code/question-code.component';
+import { QuestionLinkComponent } from './question/question-types/question-link/question-link.component';
+import { QuestionManyComponent } from './question/question-types/question-many/question-many.component';
+import { QuestionMistakesComponent } from './question/question-types/question-mistakes/question-mistakes.component';
+import { QuestionOneComponent } from './question/question-types/question-one/question-one.component';
+import { QuestionSortingComponent } from './question/question-types/question-sorting/question-sorting.component';
 
 
 @NgModule({
@@ -10,14 +22,26 @@ import { LoadingComponent } from './loading/loading.component';
     HeaderComponent,
     FooterComponent,
     LoadingComponent,
+    QuestionComponent,
+    QuestionOneComponent,
+    QuestionManyComponent,
+    QuestionCodeComponent,
+    QuestionSortingComponent,
+    QuestionMistakesComponent,
   ],
   imports: [
     CommonModule,
+    DragDropModule,
+    FormsModule,
+    BrowserModule,
+    QuestionLinkComponent,
+    CodeEditorModule.forChild(),
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     LoadingComponent,
+    QuestionComponent,
   ]
 })
 export class SharedComponentsModule { }
--- src/pages/pages.module.ts
+++ src/pages/pages.module.ts
@@ -4,7 +4,6 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { SwiperModule } from 'swiper/angular';
 import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
 import { TestPageComponent } from './test-page/test-page.component';
-import { QuestionModule } from 'src/components/question/question.module';
 import { SharedComponentsModule } from 'src/components/shared-components.module';
 //npm i swiper@8.4.7
 @NgModule({
@@ -15,7 +14,6 @@ import { SharedComponentsModule } from 'src/components/shared-components.module'
   ],
   imports: [
     CommonModule,
-    QuestionModule,
     SwiperModule,
     SharedComponentsModule,
   ],
